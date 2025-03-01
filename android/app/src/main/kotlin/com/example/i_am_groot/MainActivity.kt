423a84abc5b8feddeef6b0a91f71c0ea918e22ed
Raditya Bani Ainur Ridho <raditya.23102@mhs.unesa.ac.id>
2025-03-01 15:18:15 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/i_am_groot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.i_am_groot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("I Am Blue"),
+          backgroundColor: Colors.blue, // Warna biru
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/im_blue.png', width: 500),
+              const SizedBox(height: 30),
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.blue, // Warna biru
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Column(
+                  children: [
+                    Text("🎶 Lagu ikonik milik Eiffel 65",
+                        style: TextStyle(fontSize: 20)),
+                    Text("▶️ Musiknya catchy dan ikonik",
+                        style: TextStyle(fontSize: 20)),
+                    Text("😆 Keseringan jadi meme internet",
+                        style: TextStyle(fontSize: 20)),
+                    Text("🐂 Lembu dah beli dah belah",
+                        style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
